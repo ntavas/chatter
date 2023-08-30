603ce6b6afd760076afacf0a0fe04e4c66bb8031
Konstantinos Ntavarinos <konstantinos.ntavarinos@eurodyn.com>
2023-08-30 14:36:38 +0300

    Login and register page design

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import Add from "../img/addAvatar.png";
+
+export const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chatter</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  )
+}
